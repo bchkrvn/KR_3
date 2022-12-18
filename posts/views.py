8ef23ea1fba2a0b5b275edd59e944c9716073c54
Nikita <405663260nikita@gmail.com>
2022-12-18 22:53:03 +0300

    some little changes

--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, abort
 from .dao.posts_dao import PostDAO
 
 posts_blueprint = Blueprint('posts_blueprint', __name__, template_folder='templates')
@@ -10,8 +10,39 @@ def main_page():
     posts = post_dao.get_all_posts()
     return render_template('index.html', posts=posts)
 
+
 @posts_blueprint.route('/posts/<int:post_id>')
 def post_id_page(post_id):
-    post = post_dao.get_post_by_pk(post_id)
-    comments = post_dao.get_comments_by_post_id(post_id)
-    return render_template('post.html', post=post, comments=comments, amount_comments=len(comments))
+    try:
+        post = post_dao.get_post_by_pk(post_id)
+        comments = post_dao.get_comments_by_post_id(post_id)
+        return render_template('post.html', post=post, comments=comments, amount_comments=len(comments))
+    except ValueError:
+        abort(404)
+
+
+@posts_blueprint.route('/users/<user_name>')
+def user_page(user_name):
+    try:
+        user_posts = post_dao.get_user_posts(user_name)
+        return render_template('user-feed.html', posts=user_posts)
+    except ValueError:
+        abort(404)
+
+
+@posts_blueprint.route('/search/')
+def search_page():
+    query = request.args.get('s')
+    posts = post_dao.search_for_posts(query)
+    return render_template('search.html', posts=posts, posts_amount=len(posts), query=query)
+
+
+@posts_blueprint.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@posts_blueprint.errorhandler(500)
+def server_error(e):
+    return render_template('500.html'), 500
+
--- run.py
+++ run.py
@@ -1,13 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask
 from posts.views import posts_blueprint
 
 app = Flask(__name__)
 
 app.register_blueprint(posts_blueprint)
-
-# @app.route('/')
-# def main_page():
-#     return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
