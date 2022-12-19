8208db050e295b7c33c3617a40100f6a0f99c7c7
Nikita <405663260nikita@gmail.com>
2022-12-19 13:28:13 +0300

    add bookmark and tag

--- posts/dao/posts_dao.py
+++ posts/dao/posts_dao.py
@@ -7,7 +7,7 @@ class PostDAO:
         self.posts_path = path
         self.users_path = 'data/users.json'
         self.comments_path = 'data/comments.json'
-
+        self.bookmarks_path = 'data/bookmarks.json'
 
     def load_posts(self) -> list:
         with open(self.posts_path, encoding='utf-8') as file:
@@ -26,6 +26,16 @@ class PostDAO:
             users = json.load(file)
 
         return users
+
+    def load_bookmarks(self) -> list:
+        with open(self.bookmarks_path, encoding='utf-8') as file:
+            users = json.load(file)
+
+        return users
+
+    def save_bookmarks(self, bookmarks: list) -> None:
+        with open(self.bookmarks_path, 'w', encoding='utf-8') as file:
+            json.dump(bookmarks, file)
 
     def get_all_posts(self) -> list:
         posts = self.load_posts()
@@ -64,7 +74,43 @@ class PostDAO:
 
         for post in self.load_posts():
             if post['pk'] == pk:
-                return post
+                return self.add_hashtags(post)
 
         return dict()
 
+    def add_hashtags(self, post: dict) -> dict:
+        if '#' in post['content']:
+            content_split = post['content'].split()
+
+            for i, word in enumerate(content_split):
+                if word[0] == '#':
+                    new_word = f'<a href="/tag/{word[1:]}">#{word[1:]}</a>'
+                    content_split[i] = new_word
+
+            post['content'] = (' ').join(content_split)
+
+        return post
+
+    def add_booksmark(self, post_id: int) -> None:
+        bookmarks = self.load_bookmarks()
+        bookmarks.append(post_id)
+        self.save_bookmarks(bookmarks)
+
+    def remove_bookmark(self, post_id: int) -> None:
+        bookmarks = self.load_bookmarks()
+        bookmarks.remove(post_id)
+        self.save_bookmarks(bookmarks)
+
+    def is_in_booksmarks(self, post_id: int) -> None:
+        bookmarks = self.load_bookmarks()
+        return post_id in bookmarks
+
+    def get_posts_in_bookmarks(self) -> list:
+        bookmarks = self.load_bookmarks()
+        posts = [post for post in self.load_posts() if post['pk'] in bookmarks]
+
+        return posts
+
+    def get_amount_bookmarks(self) -> int:
+        bookmarks = self.load_bookmarks()
+        return len(bookmarks)
--- posts/views.py
+++ posts/views.py
@@ -1,15 +1,20 @@
-from flask import Blueprint, render_template, request, abort, jsonify
+import logging
+from flask import Blueprint, render_template, request, abort, jsonify, redirect
 from .dao.posts_dao import PostDAO
+
 
 posts_blueprint = Blueprint('posts_blueprint', __name__, template_folder='templates')
 post_dao = PostDAO('data/posts.json')
+logging.basicConfig(format='%(asctime)s [%(levelname)s] %(message)s',
+                    filename="api.log", level=logging.INFO, encoding='utf-8')
 
 
 @posts_blueprint.route('/')
 def main_page():
     try:
         posts = post_dao.get_all_posts()
-        return render_template('index.html', posts=posts)
+        amount_bookmarks = post_dao.get_amount_bookmarks()
+        return render_template('index.html', posts=posts,bookmarks=amount_bookmarks)
     except FileNotFoundError:
         abort(500)
 
@@ -47,8 +52,37 @@ def search_page():
         abort(500)
 
 
+@posts_blueprint.route('/tag/<tagname>')
+def tag_page(tagname):
+    query = '#' + tagname
+    posts = post_dao.search_for_posts(query)
+    return render_template('tag.html', posts=posts, tagname=tagname, posts_amount=len(posts))
+
+
+@posts_blueprint.route('/bookmarks/<int:post_id>')
+def add_bookmark(post_id):
+    if post_dao.is_in_booksmarks(post_id):
+        return redirect(f'/bookmarks/remove/{post_id}', code=302)
+    else:
+        post_dao.add_booksmark(post_id)
+        return redirect('/', code=302)
+
+
+@posts_blueprint.route('/bookmarks/remove/<int:post_id>')
+def remove_bookmark(post_id):
+    post_dao.remove_bookmark(post_id)
+    return redirect('/', code=302)
+
+
+@posts_blueprint.route('/bookmarks')
+def bookmarks():
+    posts = post_dao.get_posts_in_bookmarks()
+    return render_template('bookmarks.html', posts=posts)
+
+
 @posts_blueprint.route('/api/posts')
 def get_api_posts():
+    logging.info('Обращение к /api/posts')
     posts = post_dao.get_all_posts()
     return jsonify(posts)
 
@@ -56,6 +90,7 @@ def get_api_posts():
 @posts_blueprint.route('/api/posts/<int:post_id>')
 def get_api_post(post_id):
     posts = post_dao.get_post_by_pk(post_id)
+    logging.info(f'Обращение к /api/posts/{post_id}')
     return jsonify(posts)
 
 
