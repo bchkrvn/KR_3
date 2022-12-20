3e57a065315af14a3b8285e988659dbf11c1d099
Nikita <405663260nikita@gmail.com>
2022-12-20 22:25:22 +0300

    add description to posts_dao.py and views.py

--- posts/dao/posts_dao.py
+++ posts/dao/posts_dao.py
@@ -2,6 +2,9 @@ import json
 
 
 class PostDAO:
+    """
+    Класс для работы с данными
+    """
 
     def __init__(self, path):
         self.posts_path = path
@@ -9,39 +12,68 @@ class PostDAO:
         self.comments_path = 'data/comments.json'
         self.bookmarks_path = 'data/bookmarks.json'
 
-    def load_posts(self) -> list:
-        with open(self.posts_path, encoding='utf-8') as file:
+    def load_data(self, path):
+        """
+        Загружает данные из файла
+        :param path: ссылка на json файл
+        :return: list
+        """
+        with open(path, encoding='utf-8') as file:
             posts = json.load(file)
 
         return posts
 
-    def load_users(self) -> list:
-        with open(self.users_path, encoding='utf-8') as file:
-            users = json.load(file)
+    def load_posts(self) -> list:
+        """
+        Загружает все посты
+        :return: list
+        """
+        return self.load_data(self.posts_path)
 
-        return users
+    def load_users(self) -> list:
+        """
+        Загружает всех пользователей
+        :return: list
+        """
+        return self.load_data(self.users_path)
 
     def load_comments(self) -> list:
-        with open(self.comments_path, encoding='utf-8') as file:
-            users = json.load(file)
-
-        return users
+        """
+        Загружает все комментарии
+        :return: list
+        """
+        return self.load_data(self.comments_path)
 
     def load_bookmarks(self) -> list:
-        with open(self.bookmarks_path, encoding='utf-8') as file:
-            users = json.load(file)
-
-        return users
+        """
+        Загружает все закладки
+        :return: list
+        """
+        return self.load_data(self.bookmarks_path)
 
     def save_bookmarks(self, bookmarks: list) -> None:
+        """
+        Сохраняет закладки в json - файл
+        :param bookmarks: список закладок
+        :return: None
+        """
         with open(self.bookmarks_path, 'w', encoding='utf-8') as file:
             json.dump(bookmarks, file)
 
     def get_all_posts(self) -> list:
+        """
+        Возвращает все посты
+        :return: list
+        """
         posts = self.load_posts()
         return posts
 
     def get_user_posts(self, user_name: str) -> list:
+        """
+        Возвращает все посты пользователя с ником user_name
+        :param user_name: ник пользователя
+        :return: list
+        """
         users = self.load_users()
         if user_name not in users:
             raise ValueError(f'{user_name} is not user')
@@ -52,6 +84,11 @@ class PostDAO:
         return user_posts
 
     def get_comments_by_post_id(self, post_id: int) -> list:
+        """
+        Возвращает все комментарии под постом с id post_id
+        :param post_id: id поста, под которым нужны комментарии
+        :return: list
+        """
         posts_id = [post['pk'] for post in self.load_posts()]
         if post_id not in posts_id:
             raise ValueError(f"post with id{post_id} doesn't exist")
@@ -61,7 +98,12 @@ class PostDAO:
 
         return post_comments
 
-    def search_for_posts(self, query) -> list:
+    def search_for_posts(self, query: str) -> list:
+        """
+        Осуществляет поиск в постах по слову "query"
+        :param query: слово для поиска
+        :return: list
+        """
         posts = self.load_posts()
         query = query.lower()
         post_with_query = [post for post in posts if query in post['content'].lower()]
@@ -69,6 +111,11 @@ class PostDAO:
         return post_with_query
 
     def get_post_by_pk(self, pk: int) -> dict:
+        """
+        Возвращает пост с id = 'pk'
+        :param pk: id номер поста
+        :return: dict
+        """
         if type(pk) != int:
             raise TypeError(f'{pk} is not digit')
 
@@ -79,6 +126,11 @@ class PostDAO:
         return dict()
 
     def add_hashtags(self, post: dict) -> dict:
+        """
+        Создает ссылки из хэштегов
+        :param post: пост пользователя
+        :return: dict
+        """
         if '#' in post['content']:
             content_split = post['content'].split()
 
@@ -91,26 +143,49 @@ class PostDAO:
 
         return post
 
-    def add_booksmark(self, post_id: int) -> None:
+    def add_bookmark(self, post_id: int) -> None:
+        """
+        Добавляет пост в закладки
+        :param post_id: id поста
+        :return: None
+        """
         bookmarks = self.load_bookmarks()
         bookmarks.append(post_id)
         self.save_bookmarks(bookmarks)
 
     def remove_bookmark(self, post_id: int) -> None:
+        """
+        Удаляет пост из закладок
+        :param post_id: id поста
+        :return: None
+        """
         bookmarks = self.load_bookmarks()
         bookmarks.remove(post_id)
         self.save_bookmarks(bookmarks)
 
-    def is_in_booksmarks(self, post_id: int) -> None:
+    def is_in_bookmarks(self, post_id: int) -> bool:
+        """
+        Проверяет есть ли пост в закладках
+        :param post_id:
+        :return:
+        """
         bookmarks = self.load_bookmarks()
         return post_id in bookmarks
 
     def get_posts_in_bookmarks(self) -> list:
+        """
+        Возвращает посты, которые находятся в закладках
+        :return: list
+        """
         bookmarks = self.load_bookmarks()
         posts = [post for post in self.load_posts() if post['pk'] in bookmarks]
 
         return posts
 
     def get_amount_bookmarks(self) -> int:
+        """
+        Возвращает количество закладок
+        :return: int
+        """
         bookmarks = self.load_bookmarks()
         return len(bookmarks)
--- posts/views.py
+++ posts/views.py
@@ -5,22 +5,30 @@ from .dao.posts_dao import PostDAO
 
 posts_blueprint = Blueprint('posts_blueprint', __name__, template_folder='templates')
 post_dao = PostDAO('data/posts.json')
+
 logging.basicConfig(format='%(asctime)s [%(levelname)s] %(message)s',
                     filename="api.log", level=logging.INFO, encoding='utf-8')
 
 
 @posts_blueprint.route('/')
 def main_page():
+    '''
+    Вьюшка главной страницы со всеми постами
+    '''
     try:
         posts = post_dao.get_all_posts()
         amount_bookmarks = post_dao.get_amount_bookmarks()
-        return render_template('index.html', posts=posts,bookmarks=amount_bookmarks)
+        return render_template('index.html', posts=posts, bookmarks=amount_bookmarks)
     except FileNotFoundError:
         abort(500)
 
 
 @posts_blueprint.route('/posts/<int:post_id>')
 def post_id_page(post_id):
+    """
+    Вьюшка поста с id == post_id
+    :param post_id: id поста
+    """
     try:
         post = post_dao.get_post_by_pk(post_id)
         comments = post_dao.get_comments_by_post_id(post_id)
@@ -33,6 +41,10 @@ def post_id_page(post_id):
 
 @posts_blueprint.route('/users/<user_name>')
 def user_page(user_name):
+    """
+    Вьюшка со страницей пользователя
+    :param user_name: никнейм пользователя
+    """
     try:
         user_posts = post_dao.get_user_posts(user_name)
         return render_template('user-feed.html', posts=user_posts)
@@ -44,6 +56,9 @@ def user_page(user_name):
 
 @posts_blueprint.route('/search/')
 def search_page():
+    """
+    Вьюшка страницы поиска
+    """
     try:
         query = request.args.get('s')
         posts = post_dao.search_for_posts(query)
@@ -52,36 +67,55 @@ def search_page():
         abort(500)
 
 
-@posts_blueprint.route('/tag/<tagname>')
-def tag_page(tagname):
-    query = '#' + tagname
+@posts_blueprint.route('/tag/<tag_name>')
+def tag_page(tag_name):
+    """
+    Вьюшка страницы поиска по тегу
+    :param tag_name: тег
+    """
+    query = '#' + tag_name
     posts = post_dao.search_for_posts(query)
-    return render_template('tag.html', posts=posts, tagname=tagname, posts_amount=len(posts))
+    return render_template('tag.html', posts=posts, tagname=tag_name, posts_amount=len(posts))
 
 
 @posts_blueprint.route('/bookmarks/<int:post_id>')
 def add_bookmark(post_id):
-    if post_dao.is_in_booksmarks(post_id):
+    """
+    Вьюшка, добавляющая страницу в закладки
+    :param post_id: id поста
+    """
+    if post_dao.is_in_bookmarks(post_id):
         return redirect(f'/bookmarks/remove/{post_id}', code=302)
     else:
-        post_dao.add_booksmark(post_id)
+        post_dao.add_bookmark(post_id)
         return redirect('/', code=302)
 
 
 @posts_blueprint.route('/bookmarks/remove/<int:post_id>')
 def remove_bookmark(post_id):
+    """
+    Вьюшка, удаляющая пост из закладок
+    :param post_id:
+    """
     post_dao.remove_bookmark(post_id)
     return redirect('/', code=302)
 
 
 @posts_blueprint.route('/bookmarks')
 def bookmarks():
+    """
+    Вьюшка страницы с закладками
+    """
     posts = post_dao.get_posts_in_bookmarks()
     return render_template('bookmarks.html', posts=posts)
 
 
 @posts_blueprint.route('/api/posts')
 def get_api_posts():
+    """
+    api со всеми постами
+    :return:
+    """
     logging.info('Обращение к /api/posts')
     posts = post_dao.get_all_posts()
     return jsonify(posts)
@@ -89,6 +123,10 @@ def get_api_posts():
 
 @posts_blueprint.route('/api/posts/<int:post_id>')
 def get_api_post(post_id):
+    """
+    api поста с id post_id
+    :param post_id: id поста
+    """
     posts = post_dao.get_post_by_pk(post_id)
     logging.info(f'Обращение к /api/posts/{post_id}')
     return jsonify(posts)
@@ -96,10 +134,16 @@ def get_api_post(post_id):
 
 @posts_blueprint.errorhandler(404)
 def page_not_found(e):
+    """
+    Обработчик ошибок 404
+    """
     return render_template('404.html'), 404
 
 
 @posts_blueprint.errorhandler(500)
 def server_error(e):
+    """
+    Обработчик ошибок 500
+    """
     return render_template('500.html'), 500
 
